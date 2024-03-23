885b40803a56143b39a91df03d38732ec5d72d45
Tesla2000 <fratajczak124@gmail.com>
2024-03-23 22:18:15 +0100

    Added many text steps

--- figures/Text.py
+++ figures/Text.py
@@ -1,10 +1,13 @@
+from typing import Callable
+
 from Config import Config
 from PassedVariables import PassedVariables
 from figures.Figure import Figure
 
 
 class Text(Figure):
-    def __init__(self, width: int = None, height: int = None, x_coor: int = None, y_coor: int = None, text: str = None, font_size: str = None):
+    def __init__(self, width: int = None, height: int = None, x_coor: int = None, y_coor: int = None, text: str = None,
+                 font_size: str = None):
         super().__init__(width, height, x_coor, y_coor)
         self.font_size = font_size
         if self.font_size is None:
@@ -12,7 +15,11 @@ class Text(Figure):
         self.text = text
 
     def _draw(self, width: int, height: int):
-        PassedVariables.texts.append((self.x_coor - Config.start_x, Config.start_y - self.y_coor, self.text, self.font_size))
+        PassedVariables.texts.append(
+            (self.x_coor - Config.start_x, Config.start_y - self.y_coor, self.text, self.font_size))
+
+    def gen_add_text(self, text: str) -> Callable:
+        return lambda: setattr(self, 'text', text)
 
     @staticmethod
     def clear():
